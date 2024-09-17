9e0d6e5302c1f9eede2585ec67f6e3f9fd6ca63a
David Lim <david.lim@berkeley.edu>
2024-09-16 22:21:47 -0700

    restructured the layout, fixed some responsiveness, imported font awesome

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,14 @@
-const chatbox = document.querySelector('.chat-box');
 const chatInput = document.getElementById('chat-box__input');
 
-chatInput.addEventListener('input', () => {
-  chatInput.style.height = 'auto'; // Reset height
+chatInput.addEventListener('input', function() {
+  chatInput.style.height = '1.5em'; // Reset height if there is no scroll bar (a.k.a one line of text)
   chatInput.style.height = `${chatInput.scrollHeight}px`; // Set height based on scroll height
-  console.log('yay');
 })
+
+// setInterval(() => {
+//   const chatBox = document.querySelector('.chat-box');
+//   // console.log(chatBox.offsetWidth);
+//   // console.log(chatBox.getBoundingClientRect().width);
+
+//   console.log(window.getComputedStyle(chatBox, null).getPropertyValue("width"));
+// }, 2000)
