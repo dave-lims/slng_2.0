ada8c16207f12d8db7211bb46a52eb8984bf1f6e
David Lim <david.lim@berkeley.edu>
2024-09-20 19:56:46 -0700

    added functions to create message bubbles when user submits text

--- public/js/app.js
+++ public/js/app.js
@@ -15,19 +15,35 @@ chatInput.addEventListener("keypress", e => {
 
 chatForm.addEventListener("submit", e => {
   e.preventDefault();
-  appendMessage(chatInput.value); // TODO: now do something with this
+  appendMessage(chatInput.value);
+  chatList.scrollTo({
+    top: chatList.scrollHeight,
+    behavior: "smooth",
+  });
+  chatList.style.clear = "both";
   chatInput.value = "";
 })
 
 
-// HELPER METHODS
+/*------ HELPER METHODS -------*/
+// Adjusts the chat input box height based on the length of the text inside
 function adjustChatHeight() {
   chatInput.style.height = "1.5em"; // Reset height if there is no scroll bar (a.k.a one line of text)
   chatInput.style.height = `${chatInput.scrollHeight}px`; // Set height based on scroll height
 }
 
-function appendMessage(text) {
+// Appends a message to the chat list
+// optional parameter [bot]: default is false, if it's true, add styling for the bot bubble
+function appendMessage(text, bot=false) {
   const msgBubble = document.createElement("div");
-  msgBubble.classList.add("chat-list__bubble user");
-  console.log(msgBubble);
+  msgBubble.innerHTML = text;
+  msgBubble.classList.add("chat-list__bubble");
+
+  if (! bot) {
+    msgBubble.classList.add("userBubble");
+  } else {
+    msgBubble.classList.add("botBubble");
+  }
+
+  chatList.append(msgBubble);
 }
