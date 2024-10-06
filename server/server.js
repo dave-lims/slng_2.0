8257cd9330369acef1862d4a9067abcfea661da7
David Lim <david.lim@berkeley.edu>
2024-10-05 19:00:37 -0700

    finally... configured webpack

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-/* TODO: Change require() for import/export */
 /* TODO: learn about express, axios, cors */
 /* TODO: learn about nodemon / install? */
 
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
 const PORT = 3000;
 
@@ -11,7 +12,9 @@ const PORT = 3000;
 //   res.send(express.static('public'));
 // }
 
-app.use(express.static(path.join(__dirname, '../public')));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,10 +1,87 @@
-const path = require('path');
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
 
 module.exports = {
-  mode: 'development',  // Set to 'development' or 'production'
-  entry: './public/index.js',  // This is your entry point file
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),  // Output directory
-  },
-};
+    mode: "development",
+    entry: {
+      // define HTML files here (the key is output file w/o ext)
+      index: "./public/index.html", // => dist/index.html
+    },
+    devtool: "source-map",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            // Note: enable processing of HTML files from entry
+            {
+                test: /\.html$/,
+                loader: HtmlBundlerPlugin.loader, // HTML loader
+            },
+
+            {
+                test: /\.css$/i,
+                // Note: this plugin extracts CSS self, no style-loader needed
+                use: ["css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    // Note: define output filename of images
+                    filename: 'assets/img/[name].[hash:8][ext]',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlBundlerPlugin({
+          js: {
+            // output filename of extracted JS from source script loaded in HTML via `<script>` tag
+            filename: 'js/[name].[contenthash:8].js',
+          },
+          css: {
+            // output filename of extracted CSS from source style loaded in HTML via `<link>` tag
+            filename: 'css/[name].[contenthash:8].css',
+          },
+        }),
+    ],
+}
+
+// module.exports = {
+//   mode: 'development',  // Set to 'development' or 'production'
+//   entry: './public/index.js',  // he entry point file
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist'),  // Output directory
+//     clean: true,
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/, // Match .css files
+//         use: ['style-loader', 'css-loader'], // Use these loaders
+//       },
+//       {
+//         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+//         type: 'asset/resource',
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './public/index.html', // HTML file path
+//       filename: 'index.html', // Output filename
+//     }),
+//   ],
+
+//   // devServer: {
+//   //   static: {
+//   //     directory: path.join(__dirname, 'dist'),
+//   //   },
+//   //   port: 9000
+//   // }
+// };
